9a82ee62042c66d405d4903df8d64bc3af130377
Vicky Zhang <vickycorn.vicky@gmail.com>
2024-01-23 09:23:32 -0500

    made some more changes (not really) to the ui

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ pygame.init()
 screen = pygame.display.set_mode((600, 400))
 clock = pygame.time.Clock()
 running = True
+frametime = 0
 
 #game setup
 listofbuttons = setup.setup_buttons()
 
+testing_textbox = ui.Textbox("Somebody died, let's party", (0,0,0), (255,0,0))
 buttongroup = pygame.sprite.Group(listofbuttons)
 
 while running:
@@ -25,12 +27,12 @@ while running:
     screen.fill("grey")
 
     # RENDER YOUR GAME HERE
-    buttongroup.update()
+    buttongroup.update(frametime)
     buttongroup.draw(screen)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    frametime = clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
